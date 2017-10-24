41e2d45b6ba346c30285ac316bcbd0f1947b10c8
kobecobra76 <kobecobra76@gmail.com>
2017-10-24 16:24:20 -0500

    Added model content

--- src/main/java/com/TaskTrackerOOSDApp/model/User.java
+++ src/main/java/com/TaskTrackerOOSDApp/model/User.java
@@ -2,7 +2,7 @@ package com.TaskTrackerOOSDApp.model;
 
 //This is the model object(POJO) that helps in capturing user input
 //and passing it into different layers, and also helps retrieving 
-//data from database 
+//data from database ...
 public class User {
 		private String name;
 		private String password;

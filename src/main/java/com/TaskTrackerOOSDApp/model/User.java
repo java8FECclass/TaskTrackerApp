d6b4a4fe40ab7b88457219644eb6e3f3e1afe926
kobecobra76 <kobecobra76@gmail.com>
2017-10-24 16:19:30 -0500

    Added contents of User Model

--- src/main/java/com/TaskTrackerOOSDApp/model/User.java
+++ src/main/java/com/TaskTrackerOOSDApp/model/User.java
@@ -4,5 +4,22 @@ package com.TaskTrackerOOSDApp.model;
 //and passing it into different layers, and also helps retrieving 
 //data from database 
 public class User {
+		private String name;
+		private String password;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
 
-}
+	}
+
+	
+

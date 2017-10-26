e6f80433850d5de3b5e9f0d117ef0e491a794aaa
javaclass <javaclass@15000515cpu.fec.org>
2017-10-26 11:18:05 -0500

    added getters and setters

--- src/main/java/com/TaskTrackerOOSDApp/model/Task.java
+++ src/main/java/com/TaskTrackerOOSDApp/model/Task.java
@@ -1,10 +1,35 @@
 package com.TaskTrackerOOSDApp.model;
 // assignedTo int or ENUM?
+// this is a pojo or object or model
 
 public class Task {
 	private int taskId;
 	private String taskDescription;
 	private String status;
-	private int assignedTo;
+	private String assignedTo;
+	public int getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getAssignedTo() {
+		return assignedTo;
+	}
+	public void setAssignedTo(String assignedTo) {
+		this.assignedTo = assignedTo;
+	}
 
 }

0ae786d2eba0542d38867b1677c8652402800a50
Initech <java8FECclass>
2017-11-16 11:35:32 -0600

    updated comments

--- src/main/java/com/TaskTrackerOOSDApp/controller/TaskController.java
+++ src/main/java/com/TaskTrackerOOSDApp/controller/TaskController.java
@@ -40,7 +40,10 @@ public class TaskController {
 		model.setViewName("createTask");
 		return model;
 	}
-
+	/*
+	if admin pushes save task button then the task is created , populated
+	into task list, finally admin is redirected to admin task page
+	 */
 	@RequestMapping(value = "/saveTask", method = RequestMethod.POST)
 	public ModelAndView createTask(Task taskformobject) {
 		ModelAndView model = null;
@@ -49,7 +52,10 @@ public class TaskController {
 		return model;
 
 	}
-
+	/*
+	if employee logs into app they are redirected to employee task page
+	which would display all tasks from database
+	 */
 	@RequestMapping(value = "/empTasks/{name}")
 	public ModelAndView viewEmpTasks(ModelAndView model, @PathVariable String name ) {
 		List<Task> taskList = taskService.retrieveAll();
@@ -59,7 +65,10 @@ public class TaskController {
 		return model;
 
 	}
-	
+	/*
+	when employee selects a task it retrieves task from db, if unassigned it will show only one button " assign " ,
+	if the task is assigned to a user - two bottons will appear "In-Progress" & "Completed"
+	 */
 	@RequestMapping(value = "/viewTask/{taskId}/{name}")
 	public ModelAndView viewEmpTasks(ModelAndView model, @PathVariable Integer taskId, @PathVariable String name) {
 		Task task = taskService.retrieveTask(taskId);
@@ -69,14 +78,20 @@ public class TaskController {
 		return model;
 
 	}
-
+	/*
+	when a user pushes either In-Prgress button or Completed button , the status of the task is
+	updated in the db and reflected on task page & automatically redirects to task page
+	 */
 	@RequestMapping(value = "updateStatus/{taskId}/{status}/{name}", method=RequestMethod.GET)
 	public ModelAndView updateStatus(@PathVariable Integer taskId, @PathVariable String status, @PathVariable String name) {
 		taskService.updateStatus(taskId, status);
 		return new ModelAndView("redirect:/empTasks/" + name);
 
 	}
-	
+	/*
+	when user pushes assign button it assigns that task to the user that is logged in.
+	automatically redirects to task page
+	 */
 	@RequestMapping(value = "updateAssignedTo/{taskId}/{name}", method=RequestMethod.GET)
 	public ModelAndView updateAssignedTo(@PathVariable Integer taskId, @PathVariable String name) {
 		taskService.updateAssignedTo(taskId, name);

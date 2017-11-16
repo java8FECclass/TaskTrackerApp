408c2b174d7ccb8874bc99e893f95ab862f5668c
kobecobra76 <javadave7836@gmail.com>
2017-11-16 13:30:25 -0600

    commenting the service layer for the class

--- src/main/java/com/TaskTrackerOOSDApp/service/impl/TaskServiceImpl.java
+++ src/main/java/com/TaskTrackerOOSDApp/service/impl/TaskServiceImpl.java
@@ -18,13 +18,18 @@ public class TaskServiceImpl implements TaskService {
 	// Autowired the completed DAO to the service layer ;)
 	@Autowired
 	TaskDAO taskDAO;
-
+	
+	// 1) sets a call to the DAO layer to get a list containing all the tasks of the task database
+	// 2) returns the list to the UI level so that the tasklist page can be rendered
 	// Override is used to ensure no common errors are made when compiling
 	@Override
 	public List<Task> retrieveAll() {
 		return taskDAO.retrieveAlltasks();
 	}
-
+	
+	// 1) receives a Task object from the UI level 
+	// 2) sets default values to the assigned to and status variables (unassigned and not started)
+	// 3) calls the DAO layer to add the task to the database
 	@Override
 	public void createTask(Task task) {
 		task.setStatus(DEFAULT_STATUS);
@@ -32,7 +37,11 @@ public class TaskServiceImpl implements TaskService {
 		taskDAO.createtask(task);
 
 	}
-
+	
+	// 1) receives a Task Id and user name from the UI layer
+	// 2) calls the DAO layer with task id(task table key) to retrieve the task object
+	// 3) updates the 'assigned to' name instance variable with the user name that was passed by the UI layer
+	// 4) call the DAO layer to update the task database
 	@Override
 	public void updateAssignedTo(Integer taskId, String username) {
 		Task task = taskDAO.retrieveOne(taskId);
@@ -40,7 +49,11 @@ public class TaskServiceImpl implements TaskService {
 		taskDAO.updateTask(task);
 
 	}
-
+	
+	// 1) receives a Task Id and status from the UI layer
+	// 2) calls the DAO layer with task id(task table key) to retrieve the task object
+	// 3) updates the 'status' instance variable with the status that was passed by the UI layer
+	// 4) call the DAO layer to update the task database
 	@Override
 	public void updateStatus(Integer taskId, String status) {
 		Task task = taskDAO.retrieveOne(taskId);
@@ -48,7 +61,9 @@ public class TaskServiceImpl implements TaskService {
 		taskDAO.updateTask(task);
 
 	}
-
+	
+	// 1) receives a Task Id and status from the UI layer
+	// 2) calls the DAO layer with task id(task table key) to retrieve the task object
 	@Override
 	public Task retrieveTask(Integer taskId) {	
 		return taskDAO.retrieveOne(taskId);
--- src/main/java/com/TaskTrackerOOSDApp/service/impl/UserServiceImpl.java
+++ src/main/java/com/TaskTrackerOOSDApp/service/impl/UserServiceImpl.java
@@ -19,6 +19,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	UserDao userDao;
 	
+	// 1) Receives a User object from the UI level 2) sets a call to the DAO layer to see if there if there is a record
+	// on the user data base with a matching user name and user password 3) sends back a true or false value to the UI level which either 
+	// logs the user into the app or goes back to the logon page with an error message
 	@Override
 	public boolean isUserValid(User user) {
 		String userLoginName = user.getName();
@@ -36,7 +39,10 @@ public class UserServiceImpl implements UserService {
 		}
 
 	}
-
+	
+	// 1) Receives a User object from the UI level 2) sets a call to the DAO layer to see if there if there is a record
+	// on the user data base with matching user name 3) sends back a true or false value to the UI level which either 
+	// goes back to the logon page with an error message or continues validating the login by checking password
 	@Override
 	public boolean doesUserExist(User user) {
 		String userCheckName = user.getName();
@@ -50,7 +56,9 @@ public class UserServiceImpl implements UserService {
 		}
 
 	}
-
+	
+	// 1) Receives a User object from the UI level  2) sends back a true or false value to the UI level which either 
+	// renders either the admin or employee version of the task list page 
 	@Override
 	public boolean isUserAdmin(User user) {
 
